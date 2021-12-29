e6c8b728ff1fe016cdb6a1b95ec6d1f7637a7111
rchau <rchau@student.21-school.ru>
2021-12-29 15:59:40 +0300

    111

--- parser.c
+++ parser.c
@@ -396,42 +396,42 @@ t_msh	*ft_parser(char *str, char **env)
 	return (save_msh);
 }
 
-int main(int argc, char **argv, char **env)
-{
-	char *str;
-	t_msh *msh;
-	t_msh *tmp;
-	int i;
-	int j;
-	while (1)
-	{
-		str = readline("minishell>");
-		add_history(str);
-		msh = ft_parser(str, env);
+// int main(int argc, char **argv, char **env)
+// {
+// 	char *str;
+// 	t_msh *msh;
+// 	t_msh *tmp;
+// 	int i;
+// 	int j;
+// 	while (1)
+// 	{
+// 		str = readline("minishell>");
+// 		add_history(str);
+// 		msh = ft_parser(str, env);
 
-		j = 1;
-		while(msh)
-		{
-			printf("*** command %d ***\n", j);
-			printf("argc: %d\n", msh->argc);
-			i = 0;
-			while(i < msh->argc)
-			{
-				printf("argv[%d]: %s\n", i, msh->argv[i]);
-				free(msh->argv[i]);
-				i++;
-			}
-			free(msh->argv);
-			printf("op: %c\n", msh->op);
-			printf("fdin: %d, fdout: %d\n", msh->fdin, msh->fdout);
-			tmp = msh;
-			msh = msh->next;
-			free(tmp);
-			j++;
-		}
-		free(str);
-	}
-	return (0);
-}
+// 		j = 1;
+// 		while(msh)
+// 		{
+// 			printf("*** command %d ***\n", j);
+// 			printf("argc: %d\n", msh->argc);
+// 			i = 0;
+// 			while(i < msh->argc)
+// 			{
+// 				printf("argv[%d]: %s\n", i, msh->argv[i]);
+// 				free(msh->argv[i]);
+// 				i++;
+// 			}
+// 			free(msh->argv);
+// 			printf("op: %c\n", msh->op);
+// 			printf("fdin: %d, fdout: %d\n", msh->fdin, msh->fdout);
+// 			tmp = msh;
+// 			msh = msh->next;
+// 			free(tmp);
+// 			j++;
+// 		}
+// 		free(str);
+// 	}
+// 	return (0);
+// }
 
 
